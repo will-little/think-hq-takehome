000ee966381775684b5f992d23af747d268be598
Will Little <will.little.wl@protonmail.com>
2024-06-10 11:41:19 +1000

    feat: implement arrow decorations

--- src/modules/home-page/HomePage.tsx
+++ src/modules/home-page/HomePage.tsx
@@ -66,7 +66,12 @@ function ArrowTextFlow() {
         them.
       </p>
 
-      <div className={clsx("w-1 h-full bg-thq-text-light")}></div>
+      <svg width="auto" viewBox="0 0 16 679" className="hidden sm:block">
+        <path
+          d="M7.29292 679.707C7.68345 680.098 8.31661 680.098 8.70714 679.707L15.0711 673.343C15.4616 672.953 15.4616 672.319 15.0711 671.929C14.6806 671.538 14.0474 671.538 13.6569 671.929L8.00003 677.586L2.34318 671.929C1.95265 671.538 1.31949 671.538 0.928962 671.929C0.538437 672.319 0.538437 672.953 0.928962 673.343L7.29292 679.707ZM7 4.37114e-08L7.00003 679L9.00003 679L9 -4.37114e-08L7 4.37114e-08Z"
+          fill="currentColor"
+        />
+      </svg>
 
       <p
         className={clsx(
@@ -80,14 +85,14 @@ function ArrowTextFlow() {
         most challenging and important questions. Anchoring ourselves in the
         principles of respect, diversity and empathy helps us get there.
       </p>
-      <div
-        className={clsx(
-          "w-[1px] h-28 sm:h-full",
-          "bg-thq-text-light",
-          "self-center",
-          "block sm:hidden"
-        )}
-      ></div>
+      <div className={clsx("h-24 self-center")}>
+        <svg height="83" viewBox="0 0 16 83" className="block sm:hidden">
+          <path
+            d="M8.5 1C8.5 0.723858 8.27614 0.5 8 0.5C7.72386 0.5 7.5 0.723858 7.5 1L8.5 1ZM7.64645 84.3536C7.84171 84.5488 8.15829 84.5488 8.35356 84.3536L11.5355 81.1716C11.7308 80.9763 11.7308 80.6597 11.5355 80.4645C11.3403 80.2692 11.0237 80.2692 10.8284 80.4645L8 83.2929L5.17158 80.4645C4.97631 80.2692 4.65973 80.2692 4.46447 80.4645C4.26921 80.6597 4.26921 80.9763 4.46447 81.1716L7.64645 84.3536ZM7.5 1L7.5 84L8.5 84L8.5 1L7.5 1Z"
+            fill="white"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
