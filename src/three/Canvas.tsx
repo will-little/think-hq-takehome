5555ab0c895b7ba79366e116e276fc47d77d0d62
Will Little <will.little.wl@protonmail.com>
2024-06-10 12:36:36 +1000

    feat: set lighting

--- src/three/Canvas.tsx
+++ src/three/Canvas.tsx
@@ -5,6 +5,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { SeaKeap } from "./SeaKeep";
 
 import {
+  Bloom,
   BrightnessContrast,
   EffectComposer,
   HueSaturation,
@@ -15,7 +16,7 @@ const CanvasWrapper = () => {
   return (
     <Canvas
       camera={{
-        position: [200, 80, -150],
+        position: [200, 60, -150],
         fov: 40,
       }}
     >
@@ -24,13 +25,24 @@ const CanvasWrapper = () => {
           brightness={-0.04} // brightness. min: -1, max: 1
           contrast={0.08} // contrast: min -1, max: 1
         /> */}
+        <Bloom
+          intensity={1.0} // The bloom intensity.
+          luminanceThreshold={0.9} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+          mipmapBlur={false} // Enables or disables mipmap blur.
+        />
         <HueSaturation
-          saturation={0.2} // saturation: min 0, max 1
+          saturation={0.4} // saturation: min 0, max 1
         />
       </EffectComposer>
       <Suspense fallback={null}>
-        {/* <ambientLight castShadow={true} intensity={0.8} />
-        <pointLight position={[0, 2, 5]} intensity={200} /> */}
+        {/* <ambientLight castShadow={true} intensity={0.8} /> */}
+        <spotLight
+          decay={0}
+          castShadow={true}
+          position={[400, 60, 50]}
+          intensity={5}
+        />
         <SeaKeap scale={0.25} position={[0, -20, 0]} />
         <Environment preset="forest" />
       </Suspense>
